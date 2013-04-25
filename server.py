0984795ff4692f7bad37d852c5b070ec94b91cd0
Lane Aasen <laneaasen@gmail.com>
2013-04-25 10:34:12 -0700

    moves index.html into a template

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 
 import requests
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 @app.route('/users/<username>')
 def get_user(username):
